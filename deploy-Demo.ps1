4b0f268e0727e35fa7f143440372938af495ba8c
jsmallwood <jason.l.smallwood@gmail.com>
2019-01-25 09:37:36 -0500

    updated length of vnet name

--- deploy-Demo.ps1
+++ deploy-Demo.ps1
@@ -25,4 +25,5 @@ New-AzResourceGroupDeployment -Name 'dc-demo-1' `
     -ResourceGroupName 'RG-DomainControllers-EastUS2' `
     -TemplateParameterUri 'https://raw.githubusercontent.com/jsmallwood/azure-Demos/master/AzureActiveDirectory/ADForest/azuredeploy.parameters.json' `
     -TemplateUri 'https://raw.githubusercontent.com/jsmallwood/azure-Demos/master/AzureActiveDirectory/ADForest/azuredeploy.json' `
+    -Verbose `
     -Debug
