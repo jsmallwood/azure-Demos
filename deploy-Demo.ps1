42db3d80bc629e75a5a71c4413657dc843bf1524
jsmallwood <jason.l.smallwood@gmail.com>
2019-01-25 09:33:23 -0500

    updated templates

--- deploy-Demo.ps1
+++ deploy-Demo.ps1
@@ -19,3 +19,10 @@ New-AzureRmResourceGroupDeployment -Name 'keyVault-Deployment-1' `
 
 
 
+
+
+New-AzResourceGroupDeployment -Name 'dc-demo-1' `
+    -ResourceGroupName 'RG-DomainControllers-EastUS2' `
+    -TemplateParameterUri 'https://raw.githubusercontent.com/jsmallwood/azure-Demos/master/AzureActiveDirectory/ADForest/azuredeploy.parameters.json' `
+    -TemplateUri 'https://raw.githubusercontent.com/jsmallwood/azure-Demos/master/AzureActiveDirectory/ADForest/azuredeploy.json' `
+    -Debug
